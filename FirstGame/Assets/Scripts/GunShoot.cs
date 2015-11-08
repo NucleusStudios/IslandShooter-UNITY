359ced82325f297333777d612093695762b9a341
Ashton Tito <ashtontito@hotmail.com>
2015-11-08 15:03:18 -0800

    Game Assets

---
+++ FirstGame/Assets/Scripts/GunShoot.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
+
+[RequireComponent(typeof(GameObject))]
+public class GunShoot : MonoBehaviour {
+    
+
+    //Errors I need to fix: ************ "The Error" ****************
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15F;
+    public float range = 10000F;
+    public float gunSwitchTime = 0.0005F;
+    
+    float timer;
+    float effectsDisplayTime = 0.2f;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    
+    //ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    
+   
+    void Awake()
+    {
+        //returns the number of the shootable layer
+        //shootableMask = LayerMask.GetMask("Shootable");
+        //gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+       
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+       
+
+        GameObject akIden = GameObject.FindGameObjectWithTag("AK47");
+        GameObject umpIden = GameObject.FindGameObjectWithTag("UMP");
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+        {
+            Shoot();
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+        
+        
+    }
+
+    public void DisableEffects()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+    }
+
+    void Shoot()
+    {
+        timer = 0f;
+        gunAudio.Play();
+        gunLight.enabled = true;
+        //gunPartices.Stop();
+        //gunParticles.Play();
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+        shootRay.origin = transform.position;
+        shootRay.direction = -transform.up;
+        ////out shoot hit tells you that you hit something
+        //if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        //{
+        //    EnemyHealth enemyHealth = shootHit.collider.GetComponent<EnemyHealth>();
+        //    if (enemyHealth != null)
+        //    {
+        //        enemyHealth.TakeDamage(damagePerShot, shootHit.point);
+        //    }
+        //    gunLine.SetPosition(1, shootHit.point);
+        //}
+        //just draws out a line between 1 and 100 in the direction
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+    }
+}
---
+++ FirstGame/Assets/Scripts/ZoomIn.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomIn : MonoBehaviour {
+
+    Vector3 akHip = new Vector3(0.25F, -0.25F, 0.3F);
+    Vector3 umpHip = new Vector3(0.25F, -0.4F, 0.3F);
+    Vector3 ethanReg = new Vector3(0.25F, -2.25F, -0.35F);
+    public float UMPzoomOffsetX = -0.285F;
+    public float UMPzoomOffsetY = -0.02F;
+    public float AK47zoomOffsetX = -0.25F;
+    public float AK47zoomOffsetY = 0.055F;
+    public float zoomSpeed = 2.5F;
+    public GameObject ak47;
+    public GameObject ump45;
+    public GameObject ethan;
+    public float PosZ = 0.3F;
+
+    bool isZoomed = false;
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 akZoom = new Vector3(akHip.x + AK47zoomOffsetX, akHip.y + AK47zoomOffsetY, PosZ);
+        Vector3 umpZoom = new Vector3(umpHip.x + UMPzoomOffsetX, umpHip.y + UMPzoomOffsetY, PosZ);
+        Vector3 ethanZoomAK = new Vector3(ethanReg.x + AK47zoomOffsetX, ethanReg.y , -0.5F);
+        Vector3 ethanZoomUMP = new Vector3(ethanReg.x + UMPzoomOffsetX, ethanReg.y + UMPzoomOffsetY, -0.5F);
+
+        if (Input.GetButtonDown("Zoom") && !isZoomed)
+        {
+            if (ak47.activeInHierarchy)
+            {
+                transform.localPosition = akZoom;
+                ethan.transform.localPosition = ethanZoomAK;
+                isZoomed = true;
+            }
+            else if (ump45.activeInHierarchy)
+            {
+                transform.localPosition = umpZoom;
+                ethan.transform.localPosition = ethanZoomUMP;
+                isZoomed = true;
+            }
+
+        }
+        else if (Input.GetButtonUp("Zoom") && isZoomed)
+        {
+            if (ak47.activeInHierarchy)
+            {
+                transform.localPosition = akHip;
+                ethan.transform.localPosition = ethanReg;
+                isZoomed = false;
+            }
+            else if (ump45.activeInHierarchy)
+            {
+                transform.localPosition = umpHip;
+                ethan.transform.localPosition = ethanReg;
+                isZoomed = false;
+            }
+        }
+      
+        if (Input.GetButtonDown("Switch"))
+        {
+            if (ak47.activeInHierarchy  && !isZoomed)
+            {
+                ak47.gameObject.SetActive(false);
+                ump45.gameObject.SetActive(true);
+                
+            }
+            else if (ump45.activeInHierarchy && !isZoomed)
+            {
+                ump45.gameObject.SetActive(false);
+                ak47.gameObject.SetActive(true);
+                
+            }
+        }
+
+
+	}
+
+    
+}
